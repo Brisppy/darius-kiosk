4eee98e8d41aff12b4a9e51a23ef9acc690d901c
Brisppy <49708988+Brisppy@users.noreply.github.com>
2024-12-09 15:48:42 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# System information:
+ * Based on Debian 12.8.0
+ * Hostname is 'darius-kiosk'
+ * Default user is 'darius', password 'darius'
+ * No desktop environment installed
+ * Cage used as a 'window manager'
+ * Microsoft Edge installed and set to run inside Cage on startup.
+   * Runs as the 'cage' user.
+ * Prompts for configuration if no Internet connection detected.
---
+++ configure.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# This script performs initial setup and configuration for the Darius Kiosk.
+
+echo "Do you want to change the landing page URL? y/N"
+read MODIFY_LANDING_PAGE
+
+# perform landing page setup
+if [[ "$MODIFY_LANDING_PAGE" == "y" || "$MODIFY_LANDING_PAGE" == "Y" ]]; then
+  echo "Enter the URL of the website you wish to display on startup."
+  read LANDING_PAGE_URL
+  # create modified systemd service
+  SCRIPT_DIR=$(dirname "$0")
+  sed "s%INSERT_URL_HERE%$LANDING_PAGE_URL%g" "$SCRIPT_DIR/darius-kiosk.service" > "/etc/systemd/system/darius-kiosk.service"
+  systemctl daemon-reload
+  systemctl start darius-kiosk.service
+  systemctl enable darius-kiosk.service
+fi
+
+echo "Do you want to change the WiFi configuration? y/N"
+read MODIFY_WIFI
+
+# perform wifi setup
+if [[ "$MODIFY_WIFI" == "y" || "$MODIFY_WIFI" == "Y" ]]; then
+  # list available networks
+  nmcli dev wifi list
+
+  echo "Enter the SSID of the network you wish to join."
+  read WIFI_SSID
+  echo "Enter the password for this network."
+  read WIFI_PASS
+  nmcli dev wifi connect "$WIFI_SSID" password "$WIFI_PASS"
+fi
